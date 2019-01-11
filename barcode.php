4b8ce4b502abb356236263fd26fb77b394b5ef57
Dave Wollyung <dave.wollyung@gmail.com>
2019-01-10 21:07:52 -0500

    saved file

--- barcode.php
+++ barcode.php
@@ -1,5 +1,15 @@
 <?php
-
-
-
+header("Content-type: image/png");
+$image = imagecreate(100,40);
+$barcode_text = $_GET['barcode'];
+imagealphablending($image, true);
+imagesavealpha($image, true);
+$black = imagecolorallocate($image, 0, 0, 0);
+$font_height = 40;
+$new_width = ((strlen($string) * 20) + 41);
+$image_resized = imagecreatetruecolor($newwidth, 40);
+imagecopyresized($image_resized, $image, 0, 0, 0, 0, $new_width, 40, 10, 10);
+imagettftext($image_resized, $font_height, 0, 1, 40, $black, 'free3of9.ttf', '*'.$barcode_text.'*');
+imagepng($image_resized);
+imagedestroy($image_resized);
 ?>
