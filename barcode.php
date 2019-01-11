b81d403aa77dd4a7ae09d8925231f05c884ff050
Dave Wollyung <dave.wollyung@gmail.com>
2019-01-10 21:11:40 -0500

    bugfix

--- barcode.php
+++ barcode.php
@@ -6,7 +6,7 @@ imagealphablending($image, true);
 imagesavealpha($image, true);
 $black = imagecolorallocate($image, 0, 0, 0);
 $font_height = 40;
-$new_width = ((strlen($string) * 20) + 41);
+$new_width = ((strlen($barcode_text) * 20) + 41);
 $image_resized = imagecreatetruecolor($new_width, 40);
 imagecopyresized($image_resized, $image, 0, 0, 0, 0, $new_width, 40, 10, 10);
 imagettftext($image_resized, $font_height, 0, 1, 40, $black, 'free3of9.ttf', '*'.$barcode_text.'*');
